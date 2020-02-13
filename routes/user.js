04975f4eb1468bbbd9ef89a8b2c694372f2beddd
Zeeeey <ahumibe.ezinne@tizeti.com>
2020-02-13 16:15:33 +0100

    login route

--- routes/user.js
+++ routes/user.js
@@ -41,6 +41,29 @@ router.post('/register', (req, res) => {
     )
 })
 
+router.post('/login', (req, res) => {
+    User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+        if (user.length < 1) {
+            return res.status(401).json({ message: 'Auth failed'})
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if (err) {
+                return res.status(401).json({ message: 'Auth failed' })
+            }
+            if (result) {
+                return res.status(200).json({ message: 'Auth successful' })
+            }
+            res.status(401).json({ message: 'Auth failed' })
+        })
+    })
+    .catch(error => {
+        console.log("post login error", error)
+        res.status(500).json({ error })
+    })
+})
+
 router.delete('/:id', (req, res) => {
     const id = req.params.id;
     User.findOneAndRemove({ _id: id })
