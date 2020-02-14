306d9388457315f1f05c1df7cf5208f2c11a7c68
Zeeeey <ahumibe.ezinne@tizeti.com>
2020-02-14 14:22:55 +0100

    installed cookie-parser and created a withAuth middleware

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose                  =   require('mongoose'),
       express                   =   require('express'),
       kpiRoutes                 =   require('./routes/kpi'),
       userRoutes                =   require('./routes/user'),
+      cookieParser              =   require('cookie-parser'),
+      withAuth                  =   require('./middleware/mid-auth')
       app                       =   express();
 
 mongoose.connect(
@@ -17,8 +19,18 @@ mongoose.connect(
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 
 app.use(userRoutes)
 app.use('/kpis', kpiRoutes);
+
+app.get('/api/home', function(req, res) {
+    res.send('Welcome!');
+});
+
+app.get('/api/secret', function(req, res) {
+    res.send('The password is potato');
+});
+
 
 app.listen(5000, () => console.log("server started..."))
