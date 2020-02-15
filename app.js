2e3def47d36b91ec2f70f63c641314080b6bfce7
Zeeeey <ahumibe.ezinne@tizeti.com>
2020-02-15 12:50:49 +0100

    removed withauth middleware

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const mongoose                  =   require('mongoose'),
       express                   =   require('express'),
       kpiRoutes                 =   require('./routes/kpi'),
       userRoutes                =   require('./routes/user'),
-      cookieParser              =   require('cookie-parser'),
-      withAuth                  =   require('./middleware/mid-auth')
+    //   cookieParser              =   require('cookie-parser'),
       app                       =   express();
 
 mongoose.connect(
@@ -19,18 +18,9 @@ mongoose.connect(
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 app.use(userRoutes)
 app.use('/kpis', kpiRoutes);
-
-app.get('/api/home', function(req, res) {
-    res.send('Welcome!');
-});
-
-app.get('/api/secret', function(req, res) {
-    res.send('The password is potato');
-});
-
 
 app.listen(5000, () => console.log("server started..."))
